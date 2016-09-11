c1729f8a93220abbab1be05897b5bfe67971c315
Joey Ezechiëls <joey.ezechiels@gmail.com>
2016-09-11 15:31:24 +0200

    Implement exporting event data to MongoDB
    * By default it expects MongoDB @ mongodb://localhost:27017, but with
      the -m/--mongo-url option the user can specify a MongoDB instance
      somewhere else.
    * Currently all the data is collected first, and only then is it written
      to MongoDB. The reason for this is that I/O is not free, and thus can
      interfere with the accuracy of the event timestamps when done while
      collecting data.  During collection the data is stored in RAM.

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,26 @@ extern crate json;
 extern crate zmq;
 extern crate getopts;
 extern crate chrono;
+#[macro_use(bson, doc)]
+extern crate bson;
+extern crate mongodb;
+extern crate url;
 
-use chrono::*;
+use chrono::{Local, DateTime};
 use getopts::Options;
+use mongodb::{Client, ThreadedClient};
+use mongodb::coll::Collection;
+use mongodb::db::ThreadedDatabase;
 use std::env;
 use std::process;
 use std::path::PathBuf;
+use url::Url;
 
 struct Args {
     events: u64,
     output_file: PathBuf,
     quiet: bool,
+    mongodb_url: Url,
 }
 
 fn print_usage(program_name: &str, opts: &Options) {
@@ -39,6 +48,7 @@ fn parse_args() -> Args {
     let mut opts = Options::new();
     opts.optopt("e", "events", "The number of events to capture", "N");
     opts.optopt("o", "output", "Output file name", "FILE");
+    opts.optopt("m", "mongo-url", "URL used to connect to the MongoDB server", "URI");
     opts.optflag("q", "quiet", "Quiet mode i.e. no logging to stdout");
     opts.optflag("h", "help", "Print this help menu");
     let matches = match opts.parse(&args[1..]) {
@@ -60,6 +70,14 @@ fn parse_args() -> Args {
         Some(name) => name,
     };
 
+
+    let mongodb_url: Url = match matches.opt_str("mongo-url") {
+        None => Url::parse("mongodb://localhost:27017")
+            .expect("Could not parse default MongoDB server URL"),
+        Some(raw_url) => Url::parse(&raw_url)
+            .expect("Could not parse provided MongoDB server URL"),
+    };
+
     const DEFAULT_EVENT_CAPTURE_COUNT: u64 = 10;
     let event_count: u64 = match matches.opt_str("events") {
         None => DEFAULT_EVENT_CAPTURE_COUNT,
@@ -76,12 +94,13 @@ fn parse_args() -> Args {
         events: event_count,
         output_file: PathBuf::from(filename),
         quiet: matches.opt_present("quiet"),
+        mongodb_url: mongodb_url,
     }
 }
 
 fn log_entry_header(eventno: usize) {
     if eventno % HEADER_LOG_PERIOD == 1 {
-        // Log the header before every `HEADER_LOG_PERIOD` entries
+        // Log the header every `HEADER_LOG_PERIOD` entries
         info!("    {:->90}", "");
         info!("    {:^15}{:^14}{:^25}{:^20}{:>8}",
               "timestamp",
@@ -96,18 +115,61 @@ fn log_entry_header(eventno: usize) {
 /// Log a header before every X logged events.
 const HEADER_LOG_PERIOD: usize = 50;
 
-
+/// The target date/time format to be used when calling `stringify_timestamp()`.
 const TIMESTAMP_FORMAT: &'static str = "%Y-%m-%dT%H:%M:%S%.9f%z";
 
-fn jsonify_timestamp(ts: DateTime<Local>) -> String {
+fn stringify_timestamp(ts: DateTime<Local>) -> String {
     format!("{}", ts.format(TIMESTAMP_FORMAT))
 }
 
-fn unjsonify_timestamp(ts: &json::JsonValue)
-                       -> DateTime<FixedOffset> {
-    let ts_string = ts.as_str().expect("Deserializing timestamp failed");
-    DateTime::parse_from_str(ts_string, TIMESTAMP_FORMAT)
-        .expect("Deserializing timestamp failed")
+
+#[allow(unused)]
+struct SpoofaxPerfDb {
+    client: Client,
+    events: Collection,
+}
+
+impl SpoofaxPerfDb {
+    pub fn connect(url: &Url) -> Self {
+        let client = Client::with_uri(url.as_str())
+            .ok().expect("Failed to initialize MongoDB connector");
+        let coll = client.db("spoofax_perf").collection("events");
+        SpoofaxPerfDb {
+            client: client,
+            events: coll,
+        }
+    }
+
+    pub fn write_events(self, events: Vec<bson::Document>) -> Self {
+        let len = events.len();
+        match self.events.insert_many(events, None) {
+            Ok(_) => info!("Wrote {} events to MongoDB", len),
+            Err(err) => panic!("Inserting events failed: {:?}", err),
+        }
+        self
+    }
+}
+
+
+fn log_event(eventno: usize,
+             received_at: &DateTime<Local>,
+             args: &Args,
+             action: &str,
+             process: &str,
+             revision: u64) {
+    log_entry_header(eventno);
+    let timestamp = {
+        let mut timestamp = format!("{}", received_at.time());
+        while timestamp.len() < 15 { timestamp.push(' '); }
+        timestamp
+    };
+    info!("    {:-<15}   {:>3}/{}   {:^25}{:^20}{:>6}",
+          timestamp,
+          eventno, args.events,
+          action,
+          process,
+          revision
+    );
 }
 
 fn main() {
@@ -120,50 +182,42 @@ fn main() {
     let mut socket = ctx.socket(zmq::PULL).expect("Failed to create socket");
     assert!(socket.bind("tcp://*:9090").is_ok());
 
-    let mut events = array![];
     let mut msg = zmq::Message::new().unwrap();
     const WAIT: i32 = 0;
 
     info!("Writing to {}", args.output_file.display());
     info!("Capturing {} events", args.events);
+    info!("MongoDB URI:  {}", args.mongodb_url);
+
+    let mut events = vec![];
     for eventno in 1.. {
         assert_eq!(socket.recv(&mut msg, WAIT).unwrap(),  ());
         let json_str = msg.as_str().unwrap();
         let received_at: DateTime<Local> = Local::now();
-        let mut report = json::parse(json_str).unwrap();
+        let mut report: json::JsonValue = json::parse(json_str).unwrap();
         let action_json =   report["action"].take();
         let process_json =  report["process"].take();
         let revision_json = report["revision"].take();
         let action =   action_json.as_str().unwrap();
         let process =  process_json.as_str().unwrap();
         let revision = revision_json.as_u64().unwrap();
+        log_event(eventno, &received_at, &args, action, process, revision);
 
-        log_entry_header(eventno);
-        info!("    {: <15}   {:>3}/{}   {:^25}{:^20}{:>6}",
-              received_at.time(),
-              eventno, args.events,
-              action,
-              process,
-              revision
-        );
-
-        let event = object! {
+        let timestamp = stringify_timestamp(received_at);
+        let event = doc! {
             "action" => action,
             "process" => process,
             "revision" => revision,
-            "timestamp" => jsonify_timestamp(received_at)
+            "timestamp" => timestamp
         };
+        events.push(event);
 
-        // {
-        //     let ts = unjsonify_timestamp(&event["timestamp"]);
-        //     info!("timestamp = {}", ts.time());
-        // }
-
-        // info!("event: {:?}    {}", event, received_at.time());
-        assert_eq!(events.push(event).unwrap(),  ());
-        if eventno == args.events as usize {
-            break; // Exit after `args.events` events
-        }
-
+        if eventno == args.events as usize { break; }
     }
+
+    let spdb = SpoofaxPerfDb::connect(&args.mongodb_url);
+    spdb.write_events(events);
+    info!("Exiting");
 }
+
+//  LocalWords:  mongo MongoDB url perf stringify
